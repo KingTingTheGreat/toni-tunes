6544d4aa572dbf8288ae9601f9999c423d90a8fa
Jeffrey <kingtingthegreat@gmail.com>
2025-04-25 13:56:58 -0400

    dynamic header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,33 @@
+import { verifyJwt } from "@/lib/jwt";
 import { Box, Typography } from "@mui/material";
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { cookies } from "next/headers";
 import Link from "next/link";
 
-export default function Header() {
+function isSignedIn(
+  cookieStore: ReadonlyRequestCookies,
+): { signedIn: false } | { signedIn: true; picture: string | null } {
+  const d = cookieStore.get("mycookie");
+  if (!d) {
+    return { signedIn: false };
+  }
+
+  const jwtRes = verifyJwt(d.value);
+  if (!jwtRes.verified) {
+    return { signedIn: false };
+  }
+
+  if (jwtRes.claims.picture) {
+    return { signedIn: true, picture: jwtRes.claims.picture };
+  }
+
+  return { signedIn: true, picture: null };
+}
+
+export default async function Header() {
+  const cookieStore = await cookies();
+  const d = isSignedIn(cookieStore);
+
   const NavLink = ({
     href,
     children,
@@ -38,6 +64,24 @@ export default function Header() {
         </Link>
       </Typography>
       <Box component="nav" sx={{ display: "flex", padding: "0.5rem" }}>
+        {!d.signedIn ? (
+          <NavLink href="/sign-in">Sign In</NavLink>
+        ) : (
+          <NavLink href="/profile">
+            Profile
+            {/*{d.picture ? (
+              <Image
+                src={d.picture}
+                alt={"profile picture"}
+                width={40}
+                height={40}
+                style={{ borderRadius: "50%" }}
+              />
+            ) : (
+              <AccountCircle />
+            )} */}
+          </NavLink>
+        )}
         <NavLink href="/leaderboard">Leaderboard</NavLink>
         <NavLink href="/about">About</NavLink>
       </Box>
